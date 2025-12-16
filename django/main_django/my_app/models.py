122343e67d53706cb07434d08eea14a48923d6d5
khushalsinhmar <shimarsabb@gmail.com>
2025-12-17 01:53:22 +0530

    models and urls in django

--- django/main_django/my_app/models.py
+++ django/main_django/my_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class ChaiVerity(models.Model): 
+    CHAI_TYPE_CHOICE = [
+        ('ML', 'Masala Chai'),
+        ('GN', 'Ginger Chai'),
+        ('KS', 'Kasir Chai'),
+        ('PL', 'Plain Chai'),
+        ('EL', 'Elichi Chai'),
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='chais/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICE)
+    description = models.TextField(default='')
+
+    def __str__(self): 
+        return self.name 
--- django/main_django/my_app/views.py
+++ django/main_django/my_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import ChaiVerity
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def myapp_home(request): 
-    return render(request, 'myapp/app.html')
+    chais = ChaiVerity.objects.all()
+    return render(request, 'myapp/app.html', {'chais': chais})
+
+def chai_detail(request, chai_id): 
+    chai = get_object_or_404(ChaiVerity, pk = chai_id)
+    return render(request, 'myapp/chai-detail.html', {'chai': chai})
